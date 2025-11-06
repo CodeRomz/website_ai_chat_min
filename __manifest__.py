b5fecb3921162fd588d184d396575f9f6eeb42e0
CyberRoms <romualdojrb@yahoo.com>
2025-11-06 09:16:09 +0400

    .

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,7 @@
         "security/security.xml",
         "security/ir.model.access.csv",
         "views/res_config_settings_views.xml",
-        "views/ai_chat_templates.xml",
+        # "views/ai_chat_templates.xml",
     ],
     "assets": {
         "web.assets_frontend": [
